0faf79e6164f9a5829a8e184ec69790de2da7a7e
Hendrik Nunner <hendrik.nunner@next-audience.com>
2013-12-04 14:19:55 +0100

    implemented ipojo instantiation via factory/config file

--- ipojo/src/main/java/com/adviser/osgi/servlet/ipojo/IpojoServlet.java
+++ ipojo/src/main/java/com/adviser/osgi/servlet/ipojo/IpojoServlet.java
@@ -4,6 +4,8 @@
 package com.adviser.osgi.servlet.ipojo;
 
 import java.io.IOException;
+import java.util.Dictionary;
+import java.util.Enumeration;
 import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
@@ -13,25 +15,34 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.felix.ipojo.annotations.Component;
-import org.apache.felix.ipojo.annotations.Instantiate;
 import org.apache.felix.ipojo.annotations.Invalidate;
+import org.apache.felix.ipojo.annotations.Property;
 import org.apache.felix.ipojo.annotations.Requires;
+import org.apache.felix.ipojo.annotations.Updated;
 import org.apache.felix.ipojo.annotations.Validate;
 import org.osgi.service.http.HttpService;
 import org.osgi.service.http.NamespaceException;
 
 /**
- * This example shows how to retrieve the HttpService via iPOJO and register the servlet in just a few steps.
+ * This example shows how to retrieve the HttpService via iPOJO and register the servlet in just a few steps.<br>
+ * <br>
+ * The instantiation of the servlet is done by adding IPojoServlet*.cfg files into [apache-karaf-x.x.x]/deploy,
+ * containing key value pairs of the annotated properties.
  *
  * @author h.nunner
  */
 @SuppressWarnings("serial")
-@Component(immediate = true)
-@Instantiate
+@Component(name = "IPojoServlet")
 public class IpojoServlet extends HttpServlet {
 
     private static final Logger LOG = Logger.getLogger(IpojoServlet.class.getName());
-    private static final String SERVLET_ALIAS = "/ipojo";
+
+    /** servlet alias via config file injection */
+    @Property(immutable = true, mandatory = true)
+    private String servletAlias;
+    /** greeting via config file injection */
+    @Property
+    private String greeting;
 
     /** http service injection */
     @Requires
@@ -49,8 +60,8 @@ public class IpojoServlet extends HttpServlet {
      */
     @Validate
     private void start() throws ServletException, NamespaceException, Exception {
-        httpService.registerServlet(SERVLET_ALIAS, this, null, null);
-        LOG.info(getClass().getName() + "::Start");
+        httpService.registerServlet(servletAlias, this, null, null);
+        LOG.info(getClass().getName() + "::Registered servlet with alias '" + servletAlias + "'");
     }
 
     /**
@@ -58,8 +69,32 @@ public class IpojoServlet extends HttpServlet {
      */
     @Invalidate
     private void stop() {
-        httpService.unregister(SERVLET_ALIAS);
-        LOG.info(getClass().getName() + "::Stop");
+        httpService.unregister(servletAlias);
+        LOG.info(getClass().getName() + "::Deregistered servlet with alias '" + servletAlias + "'");
+    }
+
+
+    /**
+     * Callback for property changes.<br>
+     * <br>
+     * Invoked, when changes of any property in the corresponding cfg file occured.
+     *
+     * @param conf
+     *          the new property configuration - this parameter is optional
+     */
+    @Updated
+    private void updated(Dictionary<String, String> conf) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getName()).append("::Property change recognized in servlet (").append(servletAlias).append("): ");
+        Enumeration<String> keys = conf.keys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            sb.append(key).append("=").append(conf.get(key));
+            if (keys.hasMoreElements()) {
+                sb.append(", ");
+            }
+        }
+        LOG.info(sb.toString());
     }
 
 
@@ -67,7 +102,9 @@ public class IpojoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         ServletOutputStream out = res.getOutputStream();
-        out.write("Hello iPOJO!".getBytes());
+        StringBuilder sb = new StringBuilder();
+        sb.append(greeting).append(" iPOJO!");
+        out.write(sb.toString().getBytes());
         out.close();
     }
 
